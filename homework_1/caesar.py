ee5d2dbbd2a9535273643ef180bd2aafe3ea16a0
Dmirty <ddigorev@gmail.com>
2022-10-08 14:14:38 +0300

    some add: Vigenere was done; Caesar was chenge; tests was correct(delet some files); added screencast of unittests

--- homework_1/caesar.py
+++ homework_1/caesar.py
@@ -1,8 +1,6 @@
-from random import randint
-import typing as tp
-import string
+from string import ascii_uppercase, ascii_lowercase
 
-from pyparsing import sgl_quoted_string
+
 
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
@@ -17,20 +15,18 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
-    
-    alfabet = string.ascii_letters + '0123456789'
+
+    alfabet = ascii_lowercase * 2 + ascii_uppercase * 2
 
     dict = {}
-    for i in range(62):
-        if i < 62 - shift:
-            dict[alfabet[i]] = alfabet[i + shift]
-        else:
-            dict[alfabet[i]] = alfabet[i + shift - 62]
+    for letter in alfabet:
+        if letter not in dict:
+            dict[letter] = alfabet[alfabet.find(letter) + shift]
 
     ciphertext = ''
 
     for i in plaintext:
-        if (i != ' ' and i != '.' and i != ',' and i != "'" and i != "-"):
+        if i in dict.keys():
             ciphertext += dict[i]
         else: 
             ciphertext += i
@@ -52,26 +48,25 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     >>> decrypt_caesar("")
     ''
     """
-    plaintext = ""
 
-    alfabet = string.ascii_letters + '0123456789'
+    alfabet = ascii_lowercase * 2 + ascii_uppercase * 2
+
+    alfabet = alfabet[::-1]
+    
 
     dict = {}
-    for i in range(62):
-        if i > shift - 1:
-            dict[alfabet[i]] = alfabet[i - shift]
-        else:
-            dict[alfabet[i]] = alfabet[i - shift + 62]
+    for letter in alfabet:
+        if letter not in dict:
+            dict[letter] = alfabet[alfabet.find(letter) + shift]
 
     plaintext = ''
 
     for i in ciphertext:
-        if (i != ' ' and i != '.' and i != ',' and i != "'" and i != "-"):
+        if i in dict.keys():
             plaintext += dict[i]
         else: 
             plaintext += i
-
-    # PUT YOUR CODE HERE
+    
     return plaintext
 
 
@@ -89,24 +84,3 @@ def caesar_breaker_brute_force(ciphertext: str) -> int:
     # PUT YOUR CODE HERE
     return best_shift
 
-
-
-
-"""
-#############################
-Далее едет часть для проверки
-"""
-
-
-# print(string.ascii_letters)
-# print(type(string.ascii_letters))
-# print(string.ascii_letters[51])
-# print(len(string.ascii_letters))
-
-# text = "I've got a family. It is small. We are a family of four. I've got a father, a mother and a brother. I haven't got a sister. My father is.an engineer. He works in a plant. My mother is a teacher. She works at school. My brother is little. He doesn't go to school. He goes to a kindergarten. He is four.I like to play. I have got many toys. I have got a teddy-bear, dolls, a ball, a toy monkey and a doll's house. I like the doll's house very much. It is big. It is pink and nice."
-
-# shift = randint(0, 62)
-
-# encrypt = encrypt_caesar(text, shift)
-
-# print(shift, " => ", caesar_breaker_brute_force(encrypt))
--- homework_1/vigenere.py
+++ homework_1/vigenere.py
@@ -1,4 +1,108 @@
-import string
+from string import ascii_uppercase, ascii_lowercase, ascii_letters
+
+# def encrypt_vigenere(plaintext: str, keyword: str) -> str:
+#     """
+#     Encrypts plaintext using a Vigenere cipher.
+
+#     >>> encrypt_vigenere("PYTHON", "A")
+#     'PYTHON'
+#     >>> encrypt_vigenere("python", "a")
+#     'python'
+#     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
+#     'LXFOPVEFRNHR'
+#     """
+#     ciphertext = ""
+    
+#     alfabet = string.ascii_lowercase
+#     alfabet_upper = string.ascii_uppercase
+
+#     dict = {}
+#     for letter in keyword:
+#         if letter.isupper():
+#             dict[letter] = alfabet_upper.find(letter)
+#         else:
+#             dict[letter] = alfabet.find(letter)
+
+#     lene = len(alfabet)
+    
+#     i = 0
+#     N = len(keyword)
+
+#     for letter in plaintext:
+#         if i == N:
+#             i = 0
+#         if letter in dict:
+#             if letter.isupper():
+#                 sum = alfabet_upper.find(letter) + dict[keyword[i]]
+#                 if (sum < lene):
+#                     ciphertext += alfabet_upper[sum]
+#                 else: 
+#                     ciphertext += alfabet_upper[sum - lene]
+#             else:
+#                 sum = alfabet.find(letter) + dict[keyword[i]]
+#                 if (sum < lene):
+#                     ciphertext += alfabet[sum]
+#                 else: 
+#                     ciphertext += alfabet[sum - lene]
+#         else:
+#             ciphertext += letter
+
+#         i += 1
+
+#     return ciphertext
+
+
+# def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
+#     """
+#     Decrypts a ciphertext using a Vigenere cipher.
+
+#     >>> decrypt_vigenere("PYTHON", "A")
+#     'PYTHON'
+#     >>> decrypt_vigenere("python", "a")
+#     'python'
+#     >>> decrypt_vigenere("LXFOPVEFRNHR", "LEMON")
+#     'ATTACKATDAWN'
+#     """
+#     plaintext = ""
+    
+#     alfabet = string.ascii_lowercase
+#     alfabet_upper = string.ascii_uppercase
+
+#     dict = {}
+#     for letter in keyword:
+#         if letter.isupper():
+#             dict[letter] = alfabet_upper.find(letter)
+#         else:
+#             dict[letter] = alfabet.find(letter)
+
+#     lene = len(alfabet)
+    
+#     N = len(keyword)
+#     i = 0
+
+#     for letter in ciphertext:
+#         if i == N:
+#             i = 0
+
+#         print(i)
+
+#         if letter.isupper():
+#             sum = alfabet_upper.find(letter) - dict[keyword[i]]
+#             if (sum < lene):
+#                 plaintext += alfabet_upper[sum]
+#             else: 
+#                 plaintext += alfabet_upper[sum + lene]
+#         else:
+#             sum = alfabet.find(letter) + dict[keyword[i]]
+#             if (sum < lene):
+#                 plaintext += alfabet[sum]
+#             else: 
+#                 plaintext += alfabet[sum - lene]
+
+#         i += 1
+
+#     return plaintext
+
 
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
@@ -11,8 +115,30 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     >>> encrypt_vigenere("ATTACKATDAWN", "LEMON")
     'LXFOPVEFRNHR'
     """
-    ciphertext = ""
-    # PUT YOUR CODE HERE
+    ciphertext = ''
+    
+    alfabet = ascii_lowercase * 2
+    alfabet_upper = ascii_uppercase * 2
+
+    shift = [ascii_lowercase.find(i) for i in keyword.lower()]
+
+    i = 0
+    N = len(shift)
+
+    for letter in plaintext:
+        if i == N:
+            i = 0
+        if letter in ascii_letters:
+            if letter.isupper():
+                ciphertext += alfabet_upper[ascii_uppercase.find(letter) + shift[i]]
+            else:
+                ciphertext += alfabet[ascii_lowercase.find(letter) + shift[i]]
+        else:
+            ciphertext += letter
+
+
+        i += 1
+
     return ciphertext
 
 
@@ -28,5 +154,30 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    
+    alfabet = ascii_lowercase * 2
+    alfabet_upper = ascii_uppercase * 2
+
+    shift = [ascii_lowercase.find(i) for i in keyword.lower()]
+
+    i = 0
+    N = len(shift)
+
+    for letter in ciphertext:
+        if i == N:
+            i = 0
+        if letter in ascii_letters:
+            if letter.isupper():
+                plaintext += alfabet_upper[ascii_uppercase.find(letter) - shift[i]]
+            else:
+                plaintext += alfabet[ascii_lowercase.find(letter) - shift[i]]
+        else:
+            plaintext += letter
+
+
+        i += 1
+
     return plaintext
+
+
+print(decrypt_vigenere("tfvzzvwkeaqv lq aqvpzf", "lsci"))
